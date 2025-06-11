4d530f3f01711fafe2cb948245fe69e7f875dd8b
Павел Соколов <spp1602@gmail.com>
2025-06-11 10:59:24 +0300

    commands commented

--- src/Main/Commands.ts
+++ src/Main/Commands.ts
@@ -82,16 +82,17 @@ export class Commands {
     }
 
     public registerCommands() {
-        this.commands.forEach((callback, command_name) => {
-            this.plugin.addCommand(
-                {
-                    name: command_name,
-                    id: this.getCommandId(command_name),
-                    callback: async () => {
-                        await callback()
-                    }
-                }
-            )
-        });
+        return 
+        // this.commands.forEach((callback, command_name) => {
+        //     this.plugin.addCommand(
+        //         {
+        //             name: command_name,
+        //             id: this.getCommandId(command_name),
+        //             callback: async () => {
+        //                 await callback()
+        //             }
+        //         }
+        //     )
+        // });
     }
 }
